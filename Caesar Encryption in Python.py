ebb4d289b420af211c67d3645cf0ef5daa3d0fea
Md Mostafijur Rahman <104758273+hacker75msl@users.noreply.github.com>
2022-07-30 03:48:44 +0600

    Update Caesar Encryption in Python.py

--- Caesar Encryption in Python.py
+++ Caesar Encryption in Python.py
@@ -1,9 +1,9 @@
- Caesar Encryption in Python
+"""Caesar Encryption in Python
 simple Encryption in Python 
 Example:
 To begin encrypting, write your message in English on a piece of paper. For this example, we’ll encrypt the message THE SECRET PASSWORD IS ROSEBUD. Next, spin the inner wheel of the cipher wheel until its slots match up with slots in the outer wheel. Notice the dot next to the letter A in the outer wheel. Take note of the number in the inner wheel next to this dot. This is the encryption key.
 For example, in Figure 1-1, the outer circle’s A is over the inner circle’s number 8. We’ll use this encryption key to encrypt the message in our example, as shown in Figure 1-2.
-After you encrypt the entire message, the original message, THE SECRET PASSWORD IS ROSEBUD, becomes BPM AMKZMB XIAAEWZL QA ZWAMJCL. Notice that non-letter characters, such as the spaces, are not changed
+After you encrypt the entire message, the original message, THE SECRET PASSWORD IS ROSEBUD, becomes BPM AMKZMB XIAAEWZL QA ZWAMJCL. Notice that non-letter characters, such as the spaces, are not changed"""
 import string
 def Caesar(text,shift,alps):
     def shift_alp(alp):
